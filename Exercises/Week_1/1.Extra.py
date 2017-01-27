db3be61aecc63f1f02544f4ce189c97c4bda75e0
moorej22 <00jonniem@gmail.com>
2017-01-27 09:43:55 +0000

    c1 b

---
+++ Exercises/Week_1/1.Extra.py
@@ -0,0 +1,30 @@
+from threading import Thread
+import time
+
+
+F = open('poem.txt','r')
+A = F.readlines()
+
+pos = 0
+percentage = 0
+
+def reader():
+	global pos
+	global percentage
+	lines = int(len(A) * ((percentage/100)))
+	for x in range(0, lines) :
+		pos = x
+		time.sleep(.3)
+		print(A[pos])
+
+def menu():
+	global percentage
+	selection = 'n'
+	while selection != 'y':
+		selection = input('Start poem thread? (y/n)')
+		percentage = int(input('How much of the poem to read (%)'))
+	t = Thread(target=reader)
+	t.start()
+
+t2 = Thread(target=menu)
+t2.start()
