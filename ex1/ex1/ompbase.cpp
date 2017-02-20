31399cd53fd2b3f3ffc042f89dac77e33a19b08d
moorej22 <00jonniem@gmail.com>
2017-02-20 10:37:23 +0000

    pp

--- ex1/ex1/ompbase.cpp
+++ ex1/ex1/ompbase.cpp
@@ -5,13 +5,10 @@
 
 int main (void)
 {
-	
 	#pragma omp parallel
 		{
-			int i = omp_get_thread_num()
+			int i = omp_get_thread_num();
 			std::cout << omp_get_thread_num() << std::endl;
 		}
-
-
 return 0;
 }
