d65df778918320455cb8b9ab73f141f3c84d7c04
moorej22 <00jonniem@gmail.com>
2017-03-03 09:27:30 +0000

    Add

--- ex1/ex1/ompbase.cpp
+++ ex1/ex1/ompbase.cpp
@@ -5,10 +5,16 @@
 
 int main (void)
 {
-	#pragma omp parallel
-		{
-			int i = omp_get_thread_num();
-			std::cout << omp_get_thread_num() << std::endl;
+	int n = 3
+	int m = 3
+
+	#pragma omp parallel for collapse(2)
+		for(x=0;x<n;x++) {
+			for(y=0;y<m;y++) {
+				int add = x + y
+				printf(add)
+			}
 		}
+		
 return 0;
 }
